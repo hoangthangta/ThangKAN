242ad40795c7469c5eeb3ef3fbae6d2bba0ab9aa
Hoang Thang Ta <37979473+hoangthangta@users.noreply.github.com>
2024-05-08 15:02:25 +0700

    Update fmnist.py

--- fmnist.py
+++ fmnist.py
@@ -1,78 +1,111 @@
 from efficient_kan import KAN
 
-# Train on IMDb
+import pandas as pd
+import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision, torchtext
-import torchvision.transforms as transforms
+import torchtext
+#import torchvision
+#import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from sklearn.feature_extraction.text import CountVectorizer
+from datasets import load_dataset
+from sklearn.preprocessing import normalize
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-from datasets import load_dataset
 
 dataset = load_dataset('glue', 'mrpc')
-
 train_set = dataset['train']
-
 val_set = dataset['validation']
-
 test_set = dataset['test']
+print(len(train_set), len(val_set), len(test_set)) # 3668 408 1725
 
+# concat 2 sentences
 train_data = [item['sentence1'] + ' ### ' + item['sentence2']  for item in train_set]
+train_labels = [item['label']  for item in train_set]
+
 val_data = [item['sentence1'] + ' ### ' + item['sentence2']  for item in val_set]
+val_labels = [item['label']  for item in val_set]
 
-vectorizer = CountVectorizer(max_features = 5000)
-train_data = vectorizer.fit_transform(train_data)
+test_data = [item['sentence1'] + ' ### ' + item['sentence2']  for item in test_set]
+test_labels = [item['label']  for item in test_set]
 
-print(train_data[0])
+all_data = train_data + val_data + test_data
+all_labels = train_labels + val_labels + test_labels
+df_labels = pd.DataFrame(all_labels, columns=['label'])
 
-train_data = [[data, item['label']] for data, item in zip(train_data, train_set)]
+# BOW
+n_features = 20
+vectorizer = CountVectorizer(max_features = n_features*n_features) # 20*20
+BOW = vectorizer.fit_transform(all_data)
 
-val_data = vectorizer.fit_transform(val_data)
-val_data = [[data, item['label']] for data, item in zip(val_data, val_set)]
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(BOW, np.asarray(df_labels), test_size = 2133, train_size = 3668, random_state=0)
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, train_size = 408, random_state=0)
 
-trainloader = DataLoader(train_data, batch_size=64, shuffle=True)
-valloader = DataLoader(val_data, batch_size=64, shuffle=False)
+print('train shape: ', x_train.shape, y_train.shape)
+print('val shape: ', x_val.shape, y_val.shape)
+print('test shape: ', x_test.shape, y_test.shape)
 
+# concat data + labels
+#x.toarray().astype(np.float32), torch.from_numpy([x.toarray().astype(np.float32)])
 
-# Define model
-model = KAN([28 * 28, 64, 2])
+final_train = []
+for x, y in zip(x_train, y_train): final_train.append( (x.toarray().astype(np.float32).reshape(1, n_features, n_features), int(y)) )
+
+final_val = []
+for x, y in zip(x_val, y_val): final_val.append( (x.toarray().astype(np.float32).reshape(1, n_features, n_features), int(y)) )
+
+final_test = []
+for x, y in zip(x_test, y_test): final_test.append( (x.toarray().astype(np.float32).reshape(1, n_features, n_features), int(y)) )
+
+# load data
+trainloader = DataLoader(final_train, batch_size=64, shuffle=True)
+valloader = DataLoader(final_val, batch_size=64, shuffle=False)
+testloader = DataLoader(final_test, batch_size=64, shuffle=False)
+
+i = 0 
+for item in trainloader:
+    print(item[0].size())
+    if (i == 0): break
+
+# define KAN model
+model = KAN([n_features*n_features, 64, 2])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-# Define optimizer
+# define optimizer
 optimizer = optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
-# Define learning rate scheduler
+# define learning rate scheduler
 scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
 
-# Define loss
+# define loss
 criterion = nn.CrossEntropyLoss()
 best_accuracy = 0
 for epoch in range(10):
     # Train
     model.train()
     with tqdm(trainloader) as pbar:
-        for i, (images, labels) in enumerate(pbar):
-            images = images.view(-1, 28 * 28).to(device)
+        for i, (texts, labels) in enumerate(pbar):
+            texts = texts.view(-1, n_features*n_features).to(device)
             optimizer.zero_grad()
-            output = model(images)
+            output = model(texts)
             loss = criterion(output, labels.to(device))
             loss.backward()
             optimizer.step()
             accuracy = (output.argmax(dim=1) == labels.to(device)).float().mean()
             pbar.set_postfix(loss=loss.item(), accuracy=accuracy.item(), lr=optimizer.param_groups[0]['lr'])
 
-    # Validation
+    # validation
     model.eval()
     val_loss = 0
     val_accuracy = 0
     with torch.no_grad():
-        for images, labels in valloader:
-            images = images.view(-1, 28 * 28).to(device)
-            output = model(images)
+        for texts, labels in valloader:
+            texts = texts.view(-1, n_features*n_features).to(device)
+            output = model(texts)
             val_loss += criterion(output, labels.to(device)).item()
             val_accuracy += (
                 (output.argmax(dim=1) == labels.to(device)).float().mean().item()
@@ -84,7 +117,7 @@ for epoch in range(10):
         best_accuracy = val_accuracy
         torch.save(model, 'model.pth')
 
-    # Update learning rate
+    # update learning rate
     scheduler.step()
 
     print(
@@ -92,33 +125,20 @@ for epoch in range(10):
     )
 
 
-'''transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-)
-
-trainset = torchvision.datasets.FashionMNIST(
-    root="./data", train=True, download=True, transform=transform
-)
-print(trainset[0])
-valset = torchvision.datasets.FashionMNIST(
-    root="./data", train=False, download=True, transform=transform
-)
-trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
-valloader = DataLoader(valset, batch_size=64, shuffle=False)'''
-
 '''model = torch.load('model.pth')
 model.eval()
-with torch.no_grad():
-    for images, labels in valloader:
-        
-        images = images.view(-1, 28 * 28).to(device)
-        output = model(images)
-        
-        print('labels: ', labels)
-        print('output: ', output.argmax(dim=1))
-            
-        #val_accuracy += (
-        #    (output.argmax(dim=1) == labels.to(device)).float().mean().item()
-        #)
 
-'''
+criterion = nn.CrossEntropyLoss()
+test_loss = 0
+test_accuracy = 0
+with torch.no_grad():
+    for texts, labels in testloader:
+        texts = texts.view(-1, n_features*n_features).to(device)
+        output = model(texts)
+        test_loss += criterion(output, labels.to(device)).item()
+        test_accuracy += ((output.argmax(dim=1) == labels.to(device)).float().mean().item())
+
+    test_loss /= len(testloader)
+    test_accuracy /= len(testloader)
+
+print(test_accuracy, test_loss)'''
