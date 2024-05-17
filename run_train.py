6fdb92df11cc3c89e80b62caa0858bf6d2d39fe2
Hoang Thang Ta <37979473+hoangthangta@users.noreply.github.com>
2024-05-18 06:53:19 +0700

    Update run_train.py

--- run_train.py
+++ run_train.py
@@ -374,6 +374,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
     
-# ['rte', 'wnli', 'mrpc', 'cola'] 67.28 (128), 66.76 (64), 67.1 (32), 67.1 (16), 67.3 (8), 67.03 (4), 67.23 (2), 66 (1)
+# ['rte', 'wnli', 'mrpc', 'cola']
 #python run_train.py --mode "train" --network "mlp" --em_model_name "bert-base-cased" --ds_name "cola" --epochs 10 --batch_size 4 --max_len 512 --n_size 1 --m_size 768 --n_hidden 64 --n_class 2 --embed_type "pool"
 
