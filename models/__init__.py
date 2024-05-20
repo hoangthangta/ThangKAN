cfbdbbee30e1e9333063af1580bf3a150afc15f5
Hoang Thang Ta <37979473+hoangthangta@users.noreply.github.com>
2024-05-20 11:12:55 +0700

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 from .efficient_kan import EfficientKANLinear, EfficientKAN
 from .classifier import TransformerClassifier
 from .mlp import TransformerMLP
-from .wrap_kan import WrapKAN
 
-__all__ = ["EfficientKANLinear", "EfficientKAN", "TransformerClassifier", "TransformerMLP", "WrapKAN"]
+__all__ = ["EfficientKANLinear", "EfficientKAN", "TransformerClassifier", "TransformerMLP"]
