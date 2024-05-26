ddf3d53bb6c3fd0941c6d5574b2a492f44753465
Hoang Thang Ta <37979473+hoangthangta@users.noreply.github.com>
2024-05-26 14:02:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ declare -a arr2=("mrpc" "rte" "cola" "wnli")
 for i in "${arr1[@]}"; do for j in "${arr2[@]}";
 do
 	#echo "$i" "$j"
-	python run_train.py --mode "train" --network "$i" --em_model_name "bert-base-cased" --ds_name "$j" --epochs 5 --batch_size 4 --max_len 512 --n_size 1 --m_size 768 --n_hidden 64 --n_class 2 --embed_type "pool"
+	python run_train.py --mode "train" --network "$i" --em_model_name "bert-base-cased" --ds_name "$j" --epochs 10 --batch_size 4 --max_len 512 --n_size 1 --m_size 768 --n_hidden 64 --n_class 2 --embed_type "pool"
 done;
 done
